8b8d189577e82d91ca4ece3b04839518e93d344b
Sasha Markov <sasha.markov.mail@gmail.com>
2025-10-20 17:23:00 +0300

    Add solution - CodeHub

---
+++ 6-kyu/impress-your-friends-with-browns-criterion/impress-your-friends-with-browns-criterion.py
@@ -0,0 +1,11 @@
+sequence = ((1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31),
+            (2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30, 31),
+            (4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31),
+            (8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31),
+            (16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31))
+​
+def guess_number(answers):
+    return sum(sequence[i][0] for i, a in enumerate(answers) if a)
+​
+def answers_sequence(n):
+    return [int(n in tup) for tup in sequence]
