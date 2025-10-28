1bd022d143329b5688e382a36b344db361fef92b
Sasha Markov <sasha.markov.mail@gmail.com>
2025-10-28 16:41:29 +0300

    Add solution - CodeHub

---
+++ 8-kyu/collatz-conjecture-3n-plus-1/collatz-conjecture-3n-plus-1.cs
@@ -0,0 +1,15 @@
+public class Kata {
+    public static uint Hotpo( uint n ) {
+        uint steps = 0;
+        while ( n > 1 ) {
+            if ( n%2 == 0 ) {
+                n /= 2;
+            }
+            else {
+                n = 3*n + 1;
+            }
+            steps += 1;
+        }
+        return steps;
+    }
+}
