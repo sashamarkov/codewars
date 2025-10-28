576ac1579bd263738f417dec7ff0238dae3ed907
Sasha Markov <sasha.markov.mail@gmail.com>
2025-10-28 17:23:19 +0300

    Add solution - CodeHub

---
+++ 8-kyu/even-or-odd/even-or-odd.cs
@@ -0,0 +1,12 @@
+using System;
+​
+namespace Solution {
+  public class SolutionClass {
+    public static string EvenOrOdd( int number ) {
+       if( number%2 == 0 ) {
+        return "Even";
+       }
+       return "Odd";
+    }
+  }
+}
