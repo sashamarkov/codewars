169ccde278e9ac4beefd371ff9f5835879dcd9be
Sasha Markov <sasha.markov.mail@gmail.com>
2026-02-26 08:16:08 +0300

    Update solution - CodeHub

--- 7-kyu/take-gcd-until-one-of-the-piles-is-empty/take-gcd-until-one-of-the-piles-is-empty.rb
+++ 7-kyu/take-gcd-until-one-of-the-piles-is-empty/take-gcd-until-one-of-the-piles-is-empty.rb
@@ -1,10 +1,3 @@
 def calc(xs)
-  total = 0
-  while xs.min > 0
-    gcd_val = xs.reduce(:gcd)
-    qty = gcd_val * xs.min / gcd_val
-    total +=  xs.length * qty
-    xs = xs.map { |x| x - qty }
-  end
-  total
+  xs.min * xs.length
 end
