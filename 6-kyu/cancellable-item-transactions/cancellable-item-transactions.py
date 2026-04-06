950dc3552605f7e71d54f49880ca0e2839263ac7
Sasha Markov <sasha.markov.mail@gmail.com>
2026-04-06 20:54:40 +0300

    Add solution - CodeHub

---
+++ 6-kyu/cancellable-item-transactions/cancellable-item-transactions.py
@@ -0,0 +1,13 @@
+import re
+​
+def calculate(price_dict, transaction):
+    stacks = {item: [] for item in price_dict}
+    for match in re.finditer(r'(-?\d+)([A-Z])|([A-Z])', transaction, re.I):
+        if match.group(3):
+            item = match.group(3)
+            if stacks[item]:
+                stacks[item].pop()
+        else:
+            item = match.group(2)
+            stacks[item].append(int(match.group(1)))
+    return sum(sum(stack) * price_dict[item] for item, stack in stacks.items())
