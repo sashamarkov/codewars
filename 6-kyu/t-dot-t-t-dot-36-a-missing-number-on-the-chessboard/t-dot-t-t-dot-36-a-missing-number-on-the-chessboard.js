27de72fb8a474f1c25a283c4f8ad33f088c3a36a
Sasha Markov <sasha.markov.mail@gmail.com>
2026-04-07 23:35:13 +0300

    Update solution - CodeHub

--- 6-kyu/t-dot-t-t-dot-36-a-missing-number-on-the-chessboard/t-dot-t-t-dot-36-a-missing-number-on-the-chessboard.js
+++ 6-kyu/t-dot-t-t-dot-36-a-missing-number-on-the-chessboard/t-dot-t-t-dot-36-a-missing-number-on-the-chessboard.js
@@ -1,8 +1,8 @@
 function missingNumber(board1, board2) {
   let sum = 0;
   let sumSign = 1;
-  for (let i = 0; i < board1.length; i++) {
-    for (let j = 0; j < board2.length; j++) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
       const sign = (i + j) % 2 === 0 ? 1 : -1;
       sum += sign * board1[i][j];
       if (board2[i][j] !== "?") {
